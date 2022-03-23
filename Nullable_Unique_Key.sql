5604ef936e9fac520ad4bc6c44b87e4f63eb0d6d
Md. Masud Iqbal <iqbaldiit@gmail.com>
2022-03-23 17:09:07 +0600

    Update Nullable_Unique_Key.sql

--- Nullable_Unique_Key.sql
+++ Nullable_Unique_Key.sql
@@ -5,6 +5,6 @@ WHERE <unique_column_id> IS NOT NULL AND <unique_column_id>!='';
 
 --example for national ID
 CREATE UNIQUE NONCLUSTERED INDEX UC_national_id
-ON Auth.Users(national_id)
+ON dbo.Users(national_id)
 WHERE national_id IS NOT NULL AND national_id!='';
 
